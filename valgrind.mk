a5499aa59fc32e52cfce3b9e85f7f09181de926e
Thorsten Lorenz <thlorenz@gmx.de>
2014-02-04 22:37:33 -0500

    improving grind step

--- valgrind.mk
+++ valgrind.mk
@@ -1,13 +1,13 @@
 # valgrind executed on remote linux machine
 
-VFLAGS = --track-origins=yes --tool=memcheck --leak-check=yes
+VFLAGS = --track-origins=yes --tool=memcheck --leak-check=yes --error-exitcode=1
 
 rsync:
 	ssh udesktop 'rm -rf tmp/sync-stream'
 	rsync -ra -e ssh --exclude '/.git' --exclude '/bin' . udesktop:tmp/sync-stream
 
 grind: $(TESTS) 
-	for file in $^; do echo "\n\033[00;32m+++ $$file +++\033[00m\n" && valgrind $(VFLAGS) ./$$file; done
+	set -e; for file in $^; do echo "\n\033[00;32m+++ $$file +++\033[00m\n" && valgrind $(VFLAGS) ./$$file; done
 
 rgrind: rsync
 	ssh udesktop 'cd tmp/sync-stream && make clean && make grind'
